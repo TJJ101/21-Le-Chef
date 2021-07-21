03a6cc474ef2d77c244be41a3ed389c53fae163e
Darius Kong <s10207996@connect.np.edu.sg>
2021-07-21 22:25:06 +0800

    Did some stuff to the homepage and deleted the search recipe page

--- app/src/main/java/sg/edu/np/madassignment1/HomeFragment.java
+++ app/src/main/java/sg/edu/np/madassignment1/HomeFragment.java
@@ -2,18 +2,49 @@ package sg.edu.np.madassignment1;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.zip.Inflater;
 
 public class HomeFragment extends Fragment {
+
+    recipe mRecipe;
+    ArrayList<recipe> recipeList = new ArrayList<>();
+    private RecyclerView recyclerView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        for (int i = 0 ; i < 10; i++){
+            mRecipe = new recipe(
+                    "Name",
+                    "Cuisine",
+                    "Rating"
+            );
+            recipeList.add(mRecipe);
+        }
+
+        recyclerView = view.findViewById(R.id.myRecipeRecycler);
+        RecipeAdapter mAdapter = new RecipeAdapter(recipeList);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        return view;
     }
 }
--- app/src/main/java/sg/edu/np/madassignment1/MainActivity.java
+++ app/src/main/java/sg/edu/np/madassignment1/MainActivity.java
@@ -12,12 +12,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +33,6 @@ public class MainActivity extends AppCompatActivity {
         //this is to open the home fragment when creating
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
-/*        listView = findViewById(R.id.listview);
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
-        listView.setAdapter(arrayAdapter);*/
     }
 
     //to tell the nav to be selected when switching
@@ -51,36 +51,10 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.nav_add:
                     selectedFragment = new AddFragment();
                     break;
-                case R.id.nav_recipes:
-                    selectedFragment = new RecipesFragment();
-                    break;
             }
 
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
             return true;
         }
     };
-/*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-        MenuItem menuItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Search");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                arrayAdapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    }*/
 }
